b047d4290249906ef197d6a080bd5b05f5746b7c
brandonschiffer1 <brandonschiffer11@gmail.com>
2024-10-13 14:44:21 -0300

    CORS a nuestra app

--- src/main.go
+++ src/main.go
@@ -18,7 +18,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://your-web-app.com"}, // Add your web app's URL here
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
