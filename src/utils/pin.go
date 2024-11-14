b2e8be21c1fd6a43c8e92e7118bcceb2713b192a
Martin <mscazzola@fi.uba.ar>
2024-11-13 21:43:30 -0300

    cambio pin a numerico

--- src/utils/pin.go
+++ src/utils/pin.go
@@ -5,7 +5,7 @@ import (
 )
 
 func GenerateRandomString(n int) string {
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	var letters = []rune("0123456789")
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
